4ab0a0cd18c72431841ff841950956e72626ac50
mienblack <damienfelipe2015@gmail.com>
2024-05-27 16:55:11 -0300

    todolist

---
+++ Projetos/12. Todo/todo/src/Todo.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import List from "./List";
+import TodoForm from "./TodoForm";
+import "./Todo.css"
+
+function Todo(props) {
+
+    const [items, setItems] = useState([])
+
+    function onAddItem(item) {
+        setItems([...items, item])
+    }
+
+    return (
+        <div className="container">
+            <h1>Todo</h1>
+            <TodoForm onAddItem={onAddItem}></TodoForm>
+            <List items={items}></List>
+        </div>
+    )
+}
+
+export default Todo
